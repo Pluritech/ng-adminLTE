9506f827b82c1b8a978d4828479060e23915d573
mduraes1994 <mduraes1994@gmail.com>
2017-07-05 16:33:31 -0300

    scrolling to top when change a route and closing the menu if it is mobile screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      $('html,body').animate({ scrollTop: 0 }, 'slow', () => {
+        $('body').removeClass('sidebar-open');
+      });
+    });
+  }
+
 }
