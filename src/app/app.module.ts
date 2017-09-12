13594ee8f9ff664a0c2c2862a57c521aa78184be
mduraes1994 <mduraes1994@gmail.com>
2017-09-12 17:33:46 -0300

    Importing dialog module and organizing app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+// Angular Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+// Other Modules
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { AuthServiceModule } from '@pluritech/auth-service';
 import { ServerServiceModule } from '@pluritech/server-service';
@@ -14,29 +17,30 @@ import { ToasterModule } from 'angular2-toaster';
 import { PaginationModule } from '@pluritech/pagination';
 import { QuillModule } from 'ngx-quill';
 import { Ng2TableModule } from '@pluritech/ng2-responsive-table';
+import { DialogServiceModule } from '@pluritech/dialog-service';
 
-
+// Component
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashComponent } from './dash/dash.component';
 import { MainComponent } from './main/main.component';
+import { LoaderComponent } from './loader/loader.component';
 
-
+// Router config
 import { AppRoutingModule } from './app-routing.module';
 
-
+// Services
 import { LoginService } from './providers/login.service';
 
-
+// Guards
 import { UnauthenticatedGuard } from './unauthenticated.guard';
 import { AuthenticatedGuard } from './authenticated.guard';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { LoaderComponent } from './loader/loader.component';
 
-
+// Pipes
 import { DatePipe } from '@angular/common';
 
-
+// Configurations Files
 import { configuration } from './configuration';
 
 
@@ -66,7 +70,8 @@ import { configuration } from './configuration';
     FileUploadModule,
     ToasterModule,
     QuillModule,
-    Ng2TableModule
+    Ng2TableModule,
+    DialogServiceModule.forRoot()
   ],
   entryComponents: [],
   providers: [
