dbb39f0da1d5086516eb765d1818e191e7f8d6de
mduraes1994 <mduraes1994@gmail.com>
2018-03-29 14:12:43 -0300

    Add docs to listablePage

--- src/app/utils/listable-page.ts
+++ src/app/utils/listable-page.ts
@@ -3,20 +3,76 @@ import { PageEvent } from '@pluritech/pagination';
 
 export abstract class ListablePage<FilterGenericType, ListGenericType> {
 
+
+  /**
+   * This object is the filter to be sent to server.
+   * @type {FilterGenericType}
+   * @memberof ListablePage
+   */
   public filter: FilterGenericType;
+
+
+  /**
+   * The content of the page.
+   * @type {TableContent}
+   * @memberof ListablePage
+   */
   public data: TableContent;
+
+
+  /**
+   * The boolean to indicate if the list os loaded or not.
+   * @type {boolean}
+   * @memberof ListablePage
+   */
   public loaded: boolean;
+
+
+  /**
+   * The list to be showed in the page.
+   * @type {ListGenericType}
+   * @memberof ListablePage
+   */
   public list: ListGenericType;
 
-  public abstract getList(): Promise<void>;
 
+  /**
+   * This method is use to get the list from server.
+   * @abstract
+   * @param {*} [param]
+   * @returns {Promise<void>}
+   * @memberof ListablePage
+   */
+  public abstract getList(param?: any): Promise<void>;
+
+
+  /**
+   * This method is used to get the list from server with filters.
+   * @abstract
+   * @returns {Promise<void>}
+   * @memberof ListablePage
+   */
   public abstract doFilter(): Promise<void>;
 
+
+  /**
+   * This method is used to handle the page change when the user clicks on the paginator component.
+   * @protected
+   * @param {PageEvent} ev
+   * @memberof ListablePage
+   */
   protected handleChangePage(ev: PageEvent): void {
     const param = Object.assign(this.filter, ev);
-    this.getList();
+    this.getList(param);
   }
 
+
+  /**
+   * This method is used to set the content of the table that will be show to the user.
+   * @protected
+   * @abstract
+   * @memberof ListablePage
+   */
   protected abstract mountTable(): void;
 
 }
